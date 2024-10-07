d4c847540fdcf75fb2cc789dc1d3d099941f77cd
Piv Chan <pivchan@hotmail.com>
2024-10-07 11:52:01 -0700

    All three upgraded unit test projects are running

--- project/FSNUnitTests/CCnetDeploymentTests.fs
+++ project/FSNUnitTests/CCnetDeploymentTests.fs
@@ -1,27 +1,27 @@
 ﻿namespace ThoughtWorks.CruiseControl.UnitTests
 
-open System.IO;
-open Microsoft.VisualStudio.TestTools.UnitTesting;
+open System.IO
+open NUnit.Framework
 
 module Tests =
-    [<TestClass>]
+    [<TestFixture>]
     type CCnetDeploymentTests () =
 
-        [<TestMethod>]
+        [<Test>]
         member this.TestForAdminPackageOfWebDashboardIsEmpty() =
             System.Environment.CurrentDirectory <- @"C:\Users\Administrator\Desktop\dataflux\project\FSMSUnitTests";
             let configFile = System.IO.Path.Combine([|Directory.GetCurrentDirectory(); ".."; "WebDashboard"; "dashboard.config"|])
-            Assert.IsTrue(System.IO.File.Exists(configFile), $"Dashboard.config not found at {configFile}")
+            Assert.That(System.IO.File.Exists(configFile), $"Dashboard.config not found at {configFile}")
 
             let xdoc = new System.Xml.XmlDocument()
             xdoc.Load(configFile)
 
             let adminPluginNode = xdoc.SelectSingleNode("/dashboard/plugins/farmPlugins/administrationPlugin")
 
-            Assert.IsNotNull(adminPluginNode, "Admin package configuration not found in dashboard.config at {0}", configFile)
+            Assert.That(not(adminPluginNode = null), $"Admin package configuration not found in dashboard.config at {configFile}")
 
             let pwd = adminPluginNode.Attributes["password"]
 
-            Assert.IsNotNull(pwd, "password attribute not defined in admin packackage in dashboard.config at {0}", configFile)
+            Assert.That(not(pwd = null), "password attribute not defined in admin packackage in dashboard.config at {0}", configFile)
 
-            Assert.AreEqual<string>("",  pwd.Value, "Password must be empty string, to force users to enter one. No default passwords allowed in distribution", (null: obj))
+            Assert.That(pwd.Value = "", "Password must be empty string, to force users to enter one. No default passwords allowed in distribution")
