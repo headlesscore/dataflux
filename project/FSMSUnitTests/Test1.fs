af391aa593ff04aca1cc2a630503f4732cb6c375
Piv Chan <pivchan@hotmail.com>
2024-10-06 20:28:49 -0700

    Added FSMSUnitTests, FSNUnitTests, and FSxUnitTests

---
+++ project/FSMSUnitTests/Test1.fs
@@ -0,0 +1,41 @@
+﻿namespace FSMSUnitTests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type Test1 () =
+
+    [<AssemblyInitialize>]
+    static member AssemblyInit (context: TestContext) =
+        // This method is called once for the test assembly, before any tests are run.
+        ()
+
+    [<AssemblyCleanup>]
+    static member AssemblyCleanup () =
+        // This method is called once for the test assembly, after all tests are run.
+        ()
+
+    [<ClassInitialize>]
+    static member ClassInit (context: TestContext) =
+        // This method is called once for the test class, before any tests of the class are run.
+        ()
+
+    [<ClassCleanup>]
+    static member ClassCleanup () =
+        // This method is called once for the test class, after all tests of the class are run.
+        ()
+
+    [<TestInitialize>]
+    member this.TestInit () =
+        // This method is called before each test method.
+        ()
+
+    [<TestCleanup>]
+    member this.TestCleanup () =
+        // This method is called after each test method.
+        ()
+
+    [<TestMethod>]
+    member this.TestMethodPassing () =
+        Assert.IsTrue(true);
